84e790c9ef9af4cf68ce0c6d87abc062a1886710
Grigor Zyapkov <46219158+grigorzyapkov@users.noreply.github.com>
2022-02-15 15:39:57 +0200

    export ecbClient

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { BaseException } from './exceptions/BaseException';
-import ecbClient from "./client/ECBClient";
-
-ecbClient
-  .getExchangeRatesForPeriod("2022-02-02", "2022-02-02")
-  .then(rates => {
-    console.log('rates came: ' + JSON.stringify(rates));
-  })
-  .catch((err: BaseException) => {
-    console.log(err.getStatus());
-  });
+export * from "./client/ECBClient";
